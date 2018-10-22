d8a731505045d5070332182ec09387becb046621
SethOfCarso <cardona.erick@hotmail.com>
2018-10-21 20:11:08 -0500

    El coomit final del dia 21/10/2018

--- app/src/main/java/com/iteso/vizkar_v1/activityLoginScreen.java
+++ app/src/main/java/com/iteso/vizkar_v1/activityLoginScreen.java
@@ -1,14 +1,32 @@
 package com.iteso.vizkar_v1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class activityLoginScreen extends AppCompatActivity {
+
+    Button LogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        LogIn = findViewById(R.id.login);
+
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( view.getContext(), activityMain.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }
